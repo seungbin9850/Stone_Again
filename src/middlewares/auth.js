e067c78a4b706a134b241f2cb2763704346498f8
yangseungbin <tanja1206@naver.com>
2020-09-15 22:17:17 +0900

    token check middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = async (req, res, next) => {
+  const token = req.headers["access-token"];
+  if (!token) {
+    res.status(403).json({ message: "token required" });
+  }
+  try {
+    await jwt.verify(token, req.app.get("jwt-secret"), (err, decoded) => {
+      if (err) throw new Error(err);
+      req.decoded = decoded;
+      next();
+    });
+  } catch (e) {
+    res.status(401).json({ message: e.message });
+  }
+};
+
+module.exports = authMiddleware;
