09e91145d935bdf1423cb85c8e53dd8d00511689
yangseungbin <tanja1206@naver.com>
2020-09-22 18:59:42 +0900

    project init

--- src/models/index.js
+++ src/models/index.js
@@ -2,7 +2,12 @@ const Sequelize = require("sequelize");
 const config = require("../config/config");
 const db = {};
 
-const sequelize = new Sequelize({ config }, config);
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
