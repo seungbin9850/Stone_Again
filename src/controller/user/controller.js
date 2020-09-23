2b1f7dd243245fcba60fa9f007fcbfdbe02888e1
yangseungbin <tanja1206@naver.com>
2020-09-23 18:26:51 +0900

    user controller

--- src/controller/user/controller.js
+++ src/controller/user/controller.js
@@ -1,6 +1,6 @@
 const { User } = require("../../models/");
-const jwt = require("jsonwebtoken");
 const query = require("./query");
+const mkToken = require("./mkToken");
 
 const register = async (req, res, next) => {
   const { userId, password, name } = req.body;
@@ -13,6 +13,21 @@ const register = async (req, res, next) => {
   }
 };
 
+const login = async (req, res, next) => {
+  const { userId, password } = req.body;
+  try {
+    const user = await query.findOneByUserId(userId);
+    if (!(await query.passwordCompare(password, user.password)))
+      throw new Error("존재하지 않는 유저");
+    const accessToken = await mkToken.mkAccess(req, user);
+    const refreshToken = await mkToken.mkRefresh(req, user);
+    res.status(200).json({ accessToken, refreshToken }).end();
+  } catch (e) {
+    res.status(404).end();
+  }
+};
+
 module.exports = {
   register,
+  login,
 };
