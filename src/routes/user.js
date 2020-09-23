3d91d52b0709c8c1d439c271a94ab4ef23cb676b
yangseungbin <tanja1206@naver.com>
2020-09-23 18:27:03 +0900

    connect router

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,5 +2,6 @@ const router = require("express")();
 const controller = require("../controller/user/controller");
 
 router.post("/", controller.register);
+router.post("/login", controller.login);
 
 module.exports = router;
