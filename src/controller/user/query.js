9eb267009cf90dbcab0953891bc0a823c7725250
yangseungbin <tanja1206@naver.com>
2020-09-23 18:26:58 +0900

    user query

--- src/controller/user/query.js
+++ src/controller/user/query.js
@@ -5,6 +5,16 @@ const passwordEncoding = async (password) => {
   return await bcrypt.hashSync(password);
 };
 
+const findOneByUserId = async (userId) => {
+  return await User.findOne({ where: { userId } });
+};
+
+const passwordCompare = async (password, encoded) => {
+  return await bcrypt.compareSync(password, encoded);
+};
+
 module.exports = {
   passwordEncoding,
+  findOneByUserId,
+  passwordCompare,
 };
