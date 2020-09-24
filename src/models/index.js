4af809e8f4a351164259aa4ea5c2257209984ea4
yangseungbin <tanja1206@naver.com>
2020-09-24 12:16:44 +0900

    connect model

--- src/models/index.js
+++ src/models/index.js
@@ -13,5 +13,15 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 db.User = require("./user")(sequelize, Sequelize);
+db.Todo = require("./todo")(sequelize, Sequelize);
+db.Goal = require("./goal")(sequelize, Sequelize);
+db.Stone = require("./stone")(sequelize, Sequelize);
+
+db.User.hasOne(db.Goal, { foreignKey: "userId", targetKey: "userId" });
+db.Goal.belongsTo(db.User, { foreignKey: "userId" });
+db.User.hasMany(db.Todo, { foreignKey: "userId", targetKey: "userId" });
+db.Todo.belongsTo(db.User, { foreignKey: "userId" });
+db.User.hasOne(db.Stone, { foreignKey: "userId", targetKey: "userId" });
+db.Stone.belongsTo(db.User, { foreignKey: "userId" });
 
 module.exports = db;
