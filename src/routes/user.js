b2838ab342fb0f3fbf2e26164f216755b749e530
yangseungbin <tanja1206@naver.com>
2020-09-24 12:57:31 +0900

    [ADD] 토큰 리프레시 API

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,9 @@
 const router = require("express")();
 const controller = require("../controller/user/controller");
+const auth = require("../middlewares/auth");
 
 router.post("/", controller.register);
 router.post("/login", controller.login);
+router.get("/refresh", auth.refreshMiddleware, controller.refresh);
 
 module.exports = router;
