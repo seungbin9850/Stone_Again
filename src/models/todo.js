730364b163b849514366c2d1a9de19dbf86fd1ff
yangseungbin <tanja1206@naver.com>
2020-09-24 12:17:04 +0900

    todo model

---
+++ src/models/todo.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define("todo", {
+    do: {
+      type: DataTypes.STRING(255),
+    },
+    delete: {
+      type: DataTypes.STRING(20),
+    },
+    userId: {
+      type: DataTypes.STRING(100),
+    },
+  });
+};
