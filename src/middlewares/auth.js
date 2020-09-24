d86bda0899e49424539e4c75bcc92c124135403d
yangseungbin <tanja1206@naver.com>
2020-09-24 12:57:21 +0900

    [ADD] 토큰 검사 미들웨어

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -16,4 +16,23 @@ const authMiddleware = async (req, res, next) => {
   }
 };
 
-module.exports = authMiddleware;
+const refreshMiddleware = async (req, res, next) => {
+  const token = req.headers["refresh-token"];
+  if (!token) {
+    res.status(403).json({ message: "token required" });
+  }
+  try {
+    await jwt.verify(token, req.app.get("refresh-secret"), (err, decoded) => {
+      if (err) throw new Error(err);
+      req.decoded = decoded;
+      next();
+    });
+  } catch (e) {
+    res.status(401).json({ message: e.message });
+  }
+};
+
+module.exports = {
+  authMiddleware,
+  refreshMiddleware,
+};
