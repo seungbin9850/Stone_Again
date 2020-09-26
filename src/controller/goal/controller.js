5cd631dba13b5fb94fba9a4153da0ae9226c0941
yangseungbin <tanja1206@naver.com>
2020-09-26 15:53:07 +0900

    [ADD] 목표 설정

--- src/controller/goal/controller.js
+++ src/controller/goal/controller.js
@@ -1,12 +1,14 @@
-const { Goal } = require("../../models");
-const { Stone } = require("../../models");
-
+const { Goal, Stone, User } = require("../../models");
 const setGoal = async (req, res, next) => {
-  const { todo, deadline } = req.body;
+  const { todo, deadline, hour, minute } = req.body;
   const userId = req.decoded.userId;
   try {
     await Goal.create({ todo, deadline, userId });
     await Stone.create({ userId });
+    const user = User.findOne({ userId });
+    user.hour = hour;
+    user.minute = minute;
+    await user.save();
     res.status(200).end();
   } catch (e) {
     res.status(400).end();
