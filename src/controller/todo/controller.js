bb0c5833bbf48d1d4ff95f44a3f6ac4aa7cc14bd
yangseungbin <tanja1206@naver.com>
2020-09-27 14:47:49 +0900

    [ADD] 투두 완료

--- src/controller/todo/controller.js
+++ src/controller/todo/controller.js
@@ -1,15 +1,12 @@
-const { Todo } = require("../../models");
+const { Todo, Stone } = require("../../models");
 
 const setTodo = async (req, res, next) => {
   const todoArr = req.body.todo;
   const userId = req.decoded.userId;
   const time = req.decoded.time;
   try {
-    const todo = await Todo.findAll({ userId });
-    if (todo.length) throw new Error("이미 투두를 작성함");
-    for (let item of todoArr) {
-      console.log(item);
-      await Todo.create({ todo: item, delete: time, userId });
+    for (let todo of todoArr) {
+      await Todo.create({ todo, delete: time, userId });
     }
     res.status(200).end();
   } catch (e) {
@@ -17,6 +14,26 @@ const setTodo = async (req, res, next) => {
   }
 };
 
+const successTodo = async (req, res, next) => {
+  const userId = req.decoded.userId;
+  try {
+    const todo = await Todo.findAll({ where: { userId } });
+    if (todo.length === 0) throw new Error("투두가 없음");
+    await Todo.destroy({ where: { userId } });
+    const stone = await Stone.findOne({ where: { userId } });
+    stone.exp += Math.floor(500 / stone.left);
+    if (stone.exp >= 100) {
+      stone.level += stone.exp / 100;
+      stone.exp -= (stone.level - 1) * 100;
+    }
+    await stone.save();
+    res.status(200).end();
+  } catch (e) {
+    res.status(400).end();
+  }
+};
+
 module.exports = {
   setTodo,
+  successTodo,
 };
--- src/routes/todo.js
+++ src/routes/todo.js
@@ -3,5 +3,6 @@ const controller = require("../controller/todo/controller");
 const auth = require("../middlewares/auth");
 
 router.post("/", auth.authMiddleware, controller.setTodo);
+router.get("/success", auth.authMiddleware, controller.successTodo);
 
 module.exports = router;
