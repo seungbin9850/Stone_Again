6bc4831f1d5cabdb76ee562d5194315656ae57e8
yangseungbin <tanja1206@naver.com>
2020-09-27 14:56:56 +0900

    [ADD] 메인

--- src/routes/main.js
+++ src/routes/main.js
@@ -2,6 +2,6 @@ const router = require("express")();
 const controller = require("../controller/main/controller");
 const auth = require("../middlewares/auth");
 
-router.get("/:time", auth.authMiddleware, controller.showMain);
+router.get("/", auth.authMiddleware, controller.showMain);
 
 module.exports = router;
