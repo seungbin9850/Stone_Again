d5f621ee6be43f44ddc9233a1c3a6da7b8c3d60d
yangseungbin <tanja1206@naver.com>
2020-10-04 21:01:25 +0900

    [ADD] 나의 돌멩이

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -5,5 +5,6 @@ const auth = require("../middlewares/auth");
 router.post("/diary", auth.authMiddleware, controller.writeDiary);
 router.get("/", auth.authMiddleware, controller.showProfile);
 router.put("/time", auth.authMiddleware, controller.setTime);
+router.get("/stone", auth.authMiddleware, controller.showStone);
 
 module.exports = router;
