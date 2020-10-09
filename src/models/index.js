ba244eae33e9db22acdc04727bbc21208a18d9d6
yangseungbin <tanja1206@naver.com>
2020-10-09 19:20:24 +0900

    [ADD] encyclopedia model

--- src/models/index.js
+++ src/models/index.js
@@ -19,6 +19,7 @@ db.Stone = require("./stone")(sequelize, Sequelize);
 db.Phrase = require("./phrase")(sequelize, Sequelize);
 db.Diary = require("./diary")(sequelize, Sequelize);
 db.Check = require("./check")(sequelize, Sequelize);
+db.Encyclopedia = require("./encyclopedia")(sequelize, Sequelize);
 
 db.User.hasOne(db.Goal, { foreignKey: "userId", targetKey: "userId" });
 db.Goal.belongsTo(db.User, { foreignKey: "userId" });
@@ -30,5 +31,7 @@ db.User.hasOne(db.Diary, { foreignKey: "userId", targetKey: "userId" });
 db.Diary.belongsTo(db.User, { foreignKey: "userId" });
 db.User.hasMany(db.Check, { foreignKey: "userId", targetKey: "userId" });
 db.Check.belongsTo(db.User, { foreignKey: "userId" });
+db.User.hasMany(db.Encyclopedia, { foreignKey: "userId", targetKey: "userId" });
+db.Encyclopedia.belongsTo(db.User, { foreignKey: "userId" });
 
 module.exports = db;
